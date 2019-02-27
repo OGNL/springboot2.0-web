61623ef48cd57f7166077c6ea4dfeb64b72c75be
zhangtao <miss_null@163.com>
2019-02-27 23:29:47 +0800

    1、文件目录结构调整

--- src/main/java/com/example/web/SpringbootDemoApplication.java
+++ src/main/java/com/example/web/SpringbootDemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.springboot_demo;
+package com.example.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/example/web/controller/PageController.java
+++ src/main/java/com/example/web/controller/PageController.java
@@ -1,4 +1,4 @@
-package com.example.springboot_demo.controller;
+package com.example.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
