e9328e4d009feb5a4077165a4fa3907b8a2f11c4
zhangtao <miss_null@163.com>
2019-02-28 22:09:04 +0800

    1、新增用户信息table

--- src/main/java/com/example/controller/PageController.java
+++ src/main/java/com/example/controller/PageController.java
@@ -1,12 +1,21 @@
 package com.example.controller;
 
+import com.example.entity.UserInfo;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/index")
 public class PageController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/thymeleaf")
     public String thymeleaf(){
@@ -16,5 +25,14 @@ public class PageController {
     @GetMapping("/freemarker")
     public String freemarker(){
         return "freemarkerIndex";
+    }
+
+    @GetMapping("/user")
+    public ModelAndView toUserList(){
+        List<UserInfo> userInfoList = userService.getUserList();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("thymeleafIndex");
+        modelAndView.addObject("userList",userInfoList);
+        return modelAndView;
     }
 }
